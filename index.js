19fabde17c0227b2bb9e909d903d933b059d5f5c
Stephen Cresswell <git@stephen-cresswell.net>
2017-07-04 06:07:18 +0100

    Refactor

--- index.js
+++ index.js
@@ -5,49 +5,53 @@ var path = require('path')
 var debug = require('debug')('power-merge:index')
 var commands = require('require-all')({ dirname: path.join(__dirname, 'lib', 'commands') })
 
-function compile(_params) {
-    var params = R.mergeDeepLeft(_params || {}, {
+function compile(options) {
+    var params = applyDefaults(options)
+    var rules = preProcessRules(params.config.rules)
+    var ruleBasedMerge = R.curry(merge)(rules)
+    return applyApiWrapper(ruleBasedMerge, params.config)
+}
+
+function applyDefaults(params) {
+    return R.mergeDeepLeft(params || {}, {
         config: { async: false, variadic: true, direction: 'left', rules: [] },
         functions: {},
         actions: {}
     })
+}
 
-    var rules = R.map(function(rule) {
+function preProcessRules(rules) {
+    return R.map(function(rule) {
         return { when: rule.when || commands.test(R.T), then: rule.then }
-    }, params.config.rules)
+    }, rules)
+}
 
-    var fn = merge
-    if (params.config.direction === 'right') fn = right(fn)
-    if (params.config.variadic) fn = variadic(fn)
-    if (params.config.async) fn = asyncify(fn)
+function applyApiWrapper(fn, config) {
+    if (config.direction === 'right') fn = R.compose(fn, R.reverse)
+    if (config.variadic) fn = variadic(fn)
+    if (config.async) fn = asyncify(fn)
     return fn
+}
 
-    function merge(args) {
-        if (args.length == 0) return
-        if (args.length == 1) return args[0] // To clone or not to clone
+function merge(rules, args) {
+    if (args.length == 0) return
+    if (args.length == 1) return args[0] // To clone or not to clone
 
-        var a = args[0]
-        for (var i = 1; i < args.length; i++) {
-            var b = args[i]
-            for (var r = 0; r < rules.length; r++) {
-                var rule = rules[r]
-                var facts = {
-                    a: { value: a, type: R.type(a) },
-                    b: { value: b, type: R.type(b) }
-                }
-                if (!rule.when(facts)) continue
-                a = rule.then(a, b)
-                break
+    var a = args[0]
+    for (var i = 1; i < args.length; i++) {
+        var b = args[i]
+        for (var r = 0; r < rules.length; r++) {
+            var rule = rules[r]
+            var facts = {
+                a: { value: a, type: R.type(a) },
+                b: { value: b, type: R.type(b) }
             }
-        }
-        return a
-    }
-
-    function right(fn) {
-        return function(args) {
-            return fn(R.reverse(args))
+            if (!rule.when(facts)) continue
+            a = rule.then(a, b)
+            break
         }
     }
+    return a
 }
 
 module.exports = R.merge({ compile: compile }, commands)
