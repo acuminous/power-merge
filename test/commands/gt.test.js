04c7d2a5edabb0d562947718bcc78d37086e953c
Stephen Cresswell <git@stephen-cresswell.net>
2017-07-17 17:56:14 +0100

    gt and lt commands

---
+++ test/commands/gt.test.js
@@ -0,0 +1,36 @@
+var assert = require('chai').assert
+var gt = require('../../lib/commands/gt')
+var Context = require('../../lib/Context')
+
+describe('gt command', function() {
+
+    var context = new Context()
+
+    it('should return true when "a" value specified by the path is greater than the given value', function() {
+        var cmd = gt('a.value', 0)
+        var facts = { a: { value : 1 }, b: { value: 2 } }
+
+        assert.equal(cmd(context, facts), true)
+    })
+
+    it('should return false when "a" value specified by the path equals the given value', function() {
+        var cmd = gt('a.value', 1)
+        var facts = { a: { value : 1 }, b: { value: 2 } }
+
+        assert.equal(cmd(context, facts), false)
+    })
+
+    it('should return false when "a" value specified by the path is less than given value', function() {
+        var cmd = gt('a.value', 2)
+        var facts = { a: { value : 1 }, b: { value: 2 } }
+
+        assert.equal(cmd(context, facts), false)
+    })
+
+    it('should allow use of array paths', function() {
+        var cmd = gt(['a', 'value'], 0)
+        var facts = { a: { value : 1 }, b: { value: 2 } }
+
+        assert.equal(cmd(context, facts), true)
+    })
+})
